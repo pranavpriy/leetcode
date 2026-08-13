64ec85ae925a38cc5305d976d02a0d7362c306f1
Pranav Priy <rishu7777raj@gmail.com>
2026-08-13 15:39:24 +0530

    Time: 6 ms (68.25%), Space: 11.2 MB (17.98%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int alastseen=-1;int blastseen=-1;int clastseen=-1;
-        int  ans=0;
-        int i=0;
-        while(i<s.size()){
-            if(s[i]=='a')alastseen=i;
-            if(s[i]=='b')blastseen=i;
-            if(s[i]=='c')clastseen=i;
-            if(alastseen!=-1 && blastseen!=-1 && clastseen!=-1){
-                int firstoccurence=min(min(alastseen,blastseen),clastseen);
-                ans=ans+firstoccurence+1;
+        vector<int> count(3, 0);
+        int left = 0;
+        int total_substrings = 0;
+        
+        // Expand the window using the right pointer
+        for (int right = 0; right < s.length(); right++) {
+            count[s[right] - 'a']++;
+            
+            // min_len core: while the window is VALID, shrink it from the left
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                count[s[left] - 'a']--;
+                left++;
             }
-            i++;
+            
+            // At this point, 'left' has moved past all valid starting positions 
+            // for the current 'right' ending position. 
+            // The number of valid starting positions is exactly 'left'.
+            total_substrings += left;
         }
-        return ans;
+        
+        return total_substrings;
     }
 };
